c04cbe66242310ff34b9eeb492d8956b48bef6ce
BuildTools <unconfigured@null.spigotmc.org>
2024-06-03 13:06:06 +0200

    données en cache + plus de relations

--- fonction.py
+++ fonction.py
@@ -3,12 +3,9 @@ import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 import os
-from bs4 import BeautifulSoup as bs
 import requests
 import json
-
-
-
+from bs4 import BeautifulSoup as bs
 
 def getHtml(mot, entrant, rel):
     session = requests.Session()
@@ -35,58 +32,6 @@ def getHtml(mot, entrant, rel):
 
     return data
 
-
-
-def createTxt(mot, entrant, rel):
-    prod = getHtml(mot, entrant, rel)
-    mot = mot.replace(" ", "_")
-    mot = mot.replace("'", "")
-    # Spécifier le chemin du dossier où vous voulez enregistrer les fichiers
-    dossier = "src/txt/"
-    
-    if entrant:
-        fileTxtName = dossier + mot.replace(" ", "_")
-        if(rel == ""):
-            fileTxtName += "_e.txt"
-        else:
-            fileTxtName += "_" + rel + "_e.txt"        
-        fileTxtName = os.path.join(fileTxtName)
-    else:
-        fileTxtName = dossier + mot.replace(" ", "_")
-        if(rel == ""):
-            fileTxtName += "_s.txt"
-        else:
-            fileTxtName += "_" + rel + "_s.txt"
-        fileTxtName = os.path.join(fileTxtName)
-
-    try:
-        filesize = os.path.getsize(fileTxtName)
-    except OSError:
-        filesize = 0
-
-    if filesize == 0:
-        if entrant:
-            fileTxtName = dossier + mot.replace(" ", "_")
-            if(rel == ""):
-                fileTxtName += "_e.txt"
-            else:
-                fileTxtName += "_" + rel + "_e.txt"        
-            fileTxtName = os.path.join(fileTxtName)
-        else:
-            fileTxtName = dossier + mot.replace(" ", "_")
-            if(rel == ""):
-                fileTxtName += "_s.txt"
-            else:
-                fileTxtName += "_" + rel + "_s.txt"
-            fileTxtName = os.path.join(fileTxtName)
-
-        with open(fileTxtName, "w", encoding="utf-8") as fileTxt:
-            fileTxt.write(str(prod))
-
-    return fileTxtName
-
-
-
 def mySplit(expression):
     resultats = []
     temporaire = ""
@@ -111,8 +56,6 @@ def mySplit(expression):
 
     return resultats
 
-
-
 def createJSON(mot, entrant, rel):
     # Remplacement des caractères spéciaux dans le mot
     mot = mot.replace(" ", "_")
@@ -121,138 +64,100 @@ def createJSON(mot, entrant, rel):
     # Construction du chemin complet du fichier JSON en fonction de l'entrée et de la relation
     if entrant:
         fileJSONName = "src/json/" + mot
-        if(rel == ""):
+        if rel == "":
             fileJSONName += "_e.json"
         else:
             fileJSONName += "_" + rel + "_e.json"
     else:
         fileJSONName = "src/json/" + mot
-        if(rel == ""):
+        if rel == "":
             fileJSONName += "_s.json"
         else:
             fileJSONName += "_" + rel + "_s.json"
     
-    try:
-        # Vérification de l'existence du fichier JSON
-        filesize = os.path.getsize(fileJSONName)
-    except OSError:
-        filesize = 0
+    # Vérification de l'existence du fichier JSON
+    if os.path.exists(fileJSONName):
+        return fileJSONName
 
-    if True:
-        # Ouverture du fichier texte en lecture
-        if entrant:
-            fileTxtName = "src/txt/" + mot
-            if(rel == ""):
-                fileTxtName += "_e.txt"
-            else:
-                fileTxtName += "_" + rel + "_e.txt"
-            fileTxt = open(os.path.join(fileTxtName), "r", encoding="utf-8")
-        else:
-            fileTxtName = "src/txt/" + mot
-            if(rel == ""):
-                fileTxtName += "_s.txt"
-            else:
-                fileTxtName += "_" + rel + "_s.txt"
-            fileTxt = open(os.path.join(fileTxtName), "r", encoding="utf-8")
+    # Récupération des données HTML
+    data = getHtml(mot, entrant, rel)
+    lines = str(data).splitlines()
+
+    # Définition des champs pour les différentes parties du JSON
+    fields_nt = ['ntname']
+    fields_e = ["name", "type", "w", "formated name"]
+    fields_rt = ['trname', 'trgpname', 'rthelp']
+    fields_r = ["node1", "node2", "type", "w"]
+
+    # Initialisation des dictionnaires pour stocker les données
+    dict0 = {}
+    dict_e = {}
+    dict_rt = {}
+    dict_r = {}
+    dict_nt = {}
+
+    # Parcours des lignes du fichier texte
+    for line in lines:
+        description = list(mySplit(line.strip()))
         
-        # Lecture des lignes du fichier texte
-        lines = fileTxt.readlines()
+        if len(description) > 0:
+            if description[0] == "nt":
+                dict2 = {}
+                id = description[1]
+                if len(description) > 2:
+                    dict2[fields_nt[0]] = description[2]
+                dict_nt[id] = dict2
 
-        # Ouverture du fichier JSON en écriture
-        fileJSON = open(fileJSONName, "w", encoding="utf-8")
-
-        # Définition des champs pour les différentes parties du JSON
-        fields_nt = ['ntname']
-        fields_e = ["name", "type", "w", "formated name"]
-        fields_rt = ['trname', 'trgpname', 'rthelp']
-        fields_r = ["node1", "node2", "type", "w"]
-
-        # Initialisation des dictionnaires pour stocker les données
-        dict0 = {}
-        dict_e = {}
-        dict_rt = {}
-        dict_r = {}
-        dict_nt = {}
-
-        # Parcours des lignes du fichier texte
-        for i in range(len(lines)):
-            description = list(mySplit(lines[i].strip()))
-            
-            if len(description) > 0:
-                if description[0] == "nt":
-                    dict2 = {}
-                    id = description[1]
-                    for i in range(1):
-                        dict2[fields_nt[i]] = description[i + 2]
-                    dict_nt[id] = dict2
-
-                elif description[0] == "e":
-                    dict2 = {}
-                    id = description[1]
-                    for i in range(3):
+            elif description[0] == "e":
+                dict2 = {}
+                id = description[1]
+                for i in range(3):
+                    if len(description) > i + 2:
                         dict2[fields_e[i]] = description[i + 2]
-                    if len(description) > 5:
-                        dict2[fields_e[3]] = description[5]
-                    dict_e[id] = dict2
+                if len(description) > 5:
+                    dict2[fields_e[3]] = description[5]
+                dict_e[id] = dict2
 
-                elif description[0] == "rt":
-                    dict2 = {}
-                    id = description[1]
-                    for i in range(2):
+            elif description[0] == "rt":
+                dict2 = {}
+                id = description[1]
+                for i in range(2):
+                    if len(description) > i + 2:
                         dict2[fields_rt[i]] = description[i + 2]
-                    if len(description) > 4:
-                        dict2[fields_rt[2]] = description[4]
-                    dict_rt[id] = dict2
+                if len(description) > 4:
+                    dict2[fields_rt[2]] = description[4]
+                dict_rt[id] = dict2
 
-                elif description[0] == "r":
-                    dict2 = {}
-                    id = description[1]
-                    for i in range(4):
+            elif description[0] == "r":
+                dict2 = {}
+                id = description[1]
+                for i in range(4):
+                    if len(description) > i + 2:
                         dict2[fields_r[i]] = description[i + 2]
-                    dict_r[id] = dict2
+                dict_r[id] = dict2
 
-        # Construction du dictionnaire principal
-        dict0["nt"] = dict_nt
-        dict0["e"] = dict_e
-        dict0["r"] = dict_r
-        dict0["rt"] = dict_rt
-        
-        # Écriture du JSON dans le fichier
+    # Construction du dictionnaire principal
+    dict0["nt"] = dict_nt
+    dict0["e"] = dict_e
+    dict0["r"] = dict_r
+    dict0["rt"] = dict_rt
+    
+    # Écriture du JSON dans le fichier
+    with open(fileJSONName, "w", encoding="utf-8") as fileJSON:
         json.dump(dict0, fileJSON, indent=4)
-
-        # Fermeture des fichiers
-        fileJSON.close()
-        fileTxt.close()
 
     return fileJSONName
 
-
-
-def getData(mot,entrant,rel):
-    createTxt(mot,entrant,rel)
-    createJSON(mot,entrant,rel)
-    mot = mot.replace(" ", "_")
-    mot = mot.replace("'", "")
-    # Ouvrir le fichier  Json en lecture
-    if entrant :
-        fileJSONName = "src/json/" + mot
-        if(rel == ""):
-            fileJSONName += "_e.json"
-        else:
-            fileJSONName += "_" + rel + "_e.json"
-    else:
-        fileJSONName = "src/json/" + mot
-        if(rel == ""):
-            fileJSONName += "_s.json"
-        else:
-            fileJSONName += "_" + rel + "_s.json"
-    fileJSON = open(fileJSONName, "r")
-    data = json.load(fileJSON)
-    fileJSON.close()
+def getData(mot, entrant, rel):
+    fileJSONName = createJSON(mot, entrant, rel)
+    
+    # Ouvrir le fichier JSON en lecture
+    with open(fileJSONName, "r", encoding="utf-8") as fileJSON:
+        data = json.load(fileJSON)
     return data
 
 
-
+# Récupère id entité
 def getIdEnt(mot1, mot2, data):
     json_entities = data["e"]
     mot2_id = -1
@@ -271,7 +176,7 @@ def getIdEnt(mot1, mot2, data):
     result = {"mot2_id": mot2_id, "mot1_id": mot1_id}
     return result
 
-
+# Récupère id relation
 def getIdRel(rel, data):
     jsonDataRt = data["rt"]
     idRt = -1
@@ -283,36 +188,6 @@ def getIdRel(rel, data):
             break
     return idRt
 
-def isRelEntrante(idMot1, idRel, data):
-    jsonDataR = data["r"]
-    resultat = False
-    w = ""
-    for entity in jsonDataR:
-        x = jsonDataR[entity]['node1'].replace("'", "", 2)
-        y = jsonDataR[entity]['type'].replace("'", "", 2)
-        w = jsonDataR[entity]["w"]
-        
-        if x == idMot1 and y == idRel :
-            resultat = True
-
-            break
-    return [resultat,w]
-
-
-def isRelDeductiveInductive(dataEnt, idRelation, limit, allEntities):
-    results = []
-    compteur = 0
-
-    for entity in allEntities:
-            teste = isRelEntrante(entity[0], idRelation, dataEnt)
-            isRelE = teste[0]
-            if isRelE:
-                if "-" not in teste[1]:
-                    results.append([entity[3].replace("'", ""), entity[1].replace("'", ""), entity[2].replace("'", "")])
-            compteur += 1
-            if compteur >= limit:
-                break
-    return results
 
 def getRelEntrante(data, idRt):
     jsonDataE = data["e"]
@@ -346,36 +221,7 @@ def getRelSortante(data, idRt):
     return resultat
 
 
-
-
-# retourne les relations génériques (revoir lesquelles)
-def getGenerique(data, idMot,mot,rel):
-    dico_generalisation = {"r_isa": "6", "r_holo": "10"}
-    if rel in dico_generalisation:
-        del dico_generalisation[rel]
-    resultat = {}
-    for key in dico_generalisation:
-        resultat[key] = getRelSortante(data, dico_generalisation[key])
-
-    return resultat
-
-
-# retourne les relations spécifiques (revoir lesquelles)
-def getSpecifique(data, idMot,mot,rel):
-    dico_specialisation = {"r_hypo": "8", "r_has_part": "9"}
-    if rel in dico_specialisation:
-        del dico_specialisation[rel]
-    resultat = {}
-    for key in dico_specialisation:
-        resultat[key] = getRelSortante(data, dico_specialisation[key])
-    return resultat
-
-
-
-
-def poids(M):
-    return int(M[2])
-
+# Relation de base
 def isRelation(data, idMot1, idMot2, idRel):
     jsonDataR = data["r"]
     resultat = False
@@ -393,18 +239,108 @@ def isRelation(data, idMot1, idMot2, idRel):
             break
     return [resultat,w]
 
-def relSynonyme(data, dataEnt, limit):
+# Relations inductives / déductives / transitives
+def allRelations(data, dataEnt, dataTrois, mot1, mot2, rel, nomMot1, nomMot2, nomRel):
+    jsonDataE = data["e"]
+    jsonDataR = data["r"]
+    jsonDataEntR = dataEnt["r"]
+    jsonDataTroisR = dataTrois["r"]
+    resultat = []
+    resultatsBase = []
+    allResults = []
+
+    relationsNom = {6: "r_isa", 8: "r_hypo"}
+    relationsTransitivesNom = {10: "r_holo", 9: "r_has_part", 15: "r_lieu", 28: "r_lieu-1"}
+
+    # score selon la fiabilité de l'inférence
+    scoreInductif = 1
+    scoreDeductif = 1
+    scoreTransitif = 1
+
+    for relation in jsonDataR:
+        
+        # Si le poids est positif et que le mot est de type 1 (bon format)
+        if int(jsonDataR[relation]['w']) > 0:
+            relId = int(jsonDataR[relation]['type'])
+            # relations déductives : r_isa / XXXX
+            if relId == 6 or relId == 6:
+                node2 = jsonDataR[relation]['node2'].replace("'", "", 2)
+                resultat.append([node2, jsonDataE[node2]['name'], int(jsonDataR[relation]['w']) * scoreDeductif, relationsNom[relId]])
+            # relations inductives : r_hypo / XXXX
+            if relId == 8 or relId == 8:
+                node2 = jsonDataR[relation]['node2'].replace("'", "", 2)
+                resultat.append([node2, jsonDataE[node2]['name'], int(jsonDataR[relation]['w']) * scoreInductif, relationsNom[relId]])
+                # relation de base
+            if int(relId) == int(rel):
+                node2 = jsonDataR[relation]['node2'].replace("'", "", 2)
+                resultatsBase.append([node2, jsonDataE[node2]['name'], int(jsonDataR[relation]['w']), nomRel])
+    
+    resultat = sorted(resultat,key=poids,reverse = True)
+    # création de dico pour accéder + facilement aux données sans double boucle
+    dictionnaire = {int(item[0]): int(item[2]) for item in resultat}
+    dicoNoms = {int(item[0]): item[1] for item in resultat}
+    dicoRelations = {int(item[0]): item[3] for item in resultat}
+    dictionnaireBase = {int(item[0]): int(item[2]) for item in resultatsBase}
+    dicoNomsBase = {int(item[0]): item[1] for item in resultatsBase}
+
+    # -> inférences inductives + déductives dans le premier sens
+    # ensuite : on regarde pour chaque elt de resultats s'il y a une relation | elt rel mot2 |
+    for relation in jsonDataEntR:
+        if int(jsonDataEntR[relation]['w']) > 0:
+            relId = int(jsonDataEntR[relation]['type'])
+            node1 = jsonDataEntR[relation]['node1'].replace("'", "", 2)
+            node2 = jsonDataEntR[relation]['node2'].replace("'", "", 2)
+            if int(node1) in dictionnaire and int(relId) == int(rel):
+                explication = "| oui |" + nomMot1 + " " + dicoRelations[int(node1)] + " " + dicoNoms[int(node1)] + " & " + dicoNoms[int(node1)] + " " + nomRel + " " + nomMot2
+                score = int(jsonDataEntR[relation]['w']) + dictionnaire[int(node1)]
+                allResults.append([explication, score])
+
+        # relations transitives
+            # autoriser seulement les relations de relationsTranstivesNom (r_lieu, etc...)
+            if int(rel) in relationsTransitivesNom:
+                if int(node1) in dictionnaireBase and int(relId) == int(rel):
+                    explication = "| oui |" + nomMot1 + " " + nomRel + " " + dicoNomsBase[int(node1)] + " & " + dicoNomsBase[int(node1)] + " " + nomRel + " " + nomMot2
+                    score = (int(jsonDataEntR[relation]['w']) * scoreTransitif) + dictionnaireBase[int(node1)]
+                    allResults.append([explication, score])
+
+    # -> inférences inductives + déductives dans le deuxième sens
+    for relation in jsonDataTroisR:
+        if int(jsonDataTroisR[relation]['w']) > 0:
+            relId = int(jsonDataTroisR[relation]['type'])
+            node1 = jsonDataTroisR[relation]['node1'].replace("'", "", 2)
+            node2 = jsonDataTroisR[relation]['node2'].replace("'", "", 2)
+            # relations déductives : r_isa / XXXX
+            if relId == 6 or relId == 6:
+                if int(node2) in dictionnaire and int(node1) == int(mot2):
+                    explication = "| oui |" + nomMot1 + " " + nomRel + " " + dicoNoms[int(node2)] + " & " + nomMot2 + " " + relationsNom[relId] + " " + dicoNoms[int(node2)]
+                    score = (int(jsonDataTroisR[relation]['w']) * scoreDeductif) + dictionnaire[int(node2)]
+                    allResults.append([explication, score])
+            # relations inductives : r_hypo / XXXX
+            if relId == 8 or relId == 8:
+                if int(node2) in dictionnaire and int(node1) == int(mot1):
+                    explication = "| oui |" + nomMot1 + " " + nomRel + " " + dicoNoms[int(node2)] + " & " + nomMot2 + " " + relationsNom[relId] + " " + dicoNoms[int(node2)]
+                    score = (int(jsonDataTroisR[relation]['w'] * scoreInductif)) + dictionnaire[int(node2)]
+                    allResults.append([explication, score])    
+
+        allResults = sorted(allResults, key=lambda x: x[1], reverse=True)
+        return allResults
+
+# Relation synonyme
+def relSynonyme(data, dataEnt, relation, limit):
     idRelSyn = getIdRel("r_syn", data)
-    #synonymes = isRelSynonyme(dataEnt, idMot1, idMot2, idRelSyn, 20)
     syn1 = getRelSortante(data, idRelSyn)
     syn2 = getRelEntrante(dataEnt, idRelSyn)
-    # TODO : on itère le plus petit des deux pour gagner du temps
+    # limite de données/fichiers créés
+    if len(syn2) < limit:
+        limit = len(syn2)
     limitFiles = 5
+    if len(syn1) < limitFiles:
+        limitFiles = len(syn1)
     resultat = []
     w = ""
     for i in range(limitFiles):
         s1 = syn1[i]
-        dataEntree = getData(s1[1].replace("'", ""),True, "r_agent-1")
+        dataEntree = getData(s1[1].replace("'", ""),True, relation)
         relations = dataEntree["r"]
         for entity in relations:
             node1 = relations[entity]['node1'].replace("'", "", 2)
@@ -417,5 +353,7 @@ def relSynonyme(data, dataEnt, limit):
                     poids = int(s1[2]) + int(syn2[j][2]) + int(w)
                     resultat.append([syn1[i][1], syn2[j][1], poids])
                     break
-    #resultat = sorted(resultat,key=poids,reverse = True)
     return resultat
+
+def poids(M):
+    return int(M[2])
--- main.py
+++ main.py
@@ -1,102 +1,75 @@
 from fonction import *
+import time
 
-"""
+
 entre = input("Entrez une relation : ")
+start_time = time.time()
 
-#separer l'entrée en 3 mots 
+#separer l'entrée en 3 mots : mot1 | relation | mot2
 mots = entre.split()
 if len(mots) == 3:
-    # Le premier mot est le premier mot
     mot1 = mots[0]
-    # Le deuxième mot est la relation
     relation = mots[1]
-    # Le reste de la phrase est le deuxième mot
     mot2 = mots[2]
+
+    mot1 = mot1.replace("_"," ")
 else:
     print("Entrée invalide")
-"""
 
-# pour les tests : entre direct les mots
-mot1 = "minou"
-relation = "r_agent-1"
-mot2 = "blesser"
-
+# Récupère les données Json
 data = getData(mot1,True,"")
 dataEnt = getData(mot2,False,"")
+dataTrois = getData(mot2,True,"")
+
+# Récupère les id des mots et de la relation
 infos = getIdEnt(mot1, mot2,data)
 idMot2 = infos["mot2_id"]
 idMot1 = infos["mot1_id"]
 idRel = getIdRel(relation,data)
 
-# on récupère les entités liés par des génériques / spécifiques (ex : mot1 r_isa XX, mot1 r_hypo XX)
-genericEntites = []
-idCommuns = getGenerique(data, idMot1,mot1,relation)
-for idCommun in idCommuns:
-    for entity in idCommuns[idCommun]:
-        entity.append(idCommun)
-        genericEntites.append(entity)
-genericEntites  = sorted(genericEntites,key=poids,reverse = True)
-
-specifiquesEntites = []
-idCommuns = getSpecifique(data, idMot1,mot1,relation)
-for idCommun in idCommuns:
-    for entity in idCommuns[idCommun]:
-        entity.append(idCommun)
-        specifiquesEntites.append(entity)
-specifiquesEntites  = sorted(specifiquesEntites,key=poids,reverse = True)
-
-transEntities = getRelSortante(data, idRel)
-for tr in transEntities:
-    tr.append(relation)
-transEntities = sorted(transEntities,key=poids,reverse = True)
-
 def resteInference():
-    # relation inductive
-    induct = isRelDeductiveInductive(dataEnt, idRel, 20, specifiquesEntites)
-    for infos in induct:
-        allInferences.append([["| oui |", mot1, infos[0], infos[1], "&", infos[1], relation, mot2, "|", infos[2]], int(infos[2]), "inductive"])
+    # relation inductive / déductive / transitive
+    relations = allRelations(data, dataEnt, dataTrois, idMot1, idMot2, idRel, mot1, mot2, relation)
+    for infos in relations:
+        allInferences.append([infos[0], infos[1]])
 
-    # relation déductive
-    print("-----------------")
-    deduct = isRelDeductiveInductive(dataEnt, idRel, 20, genericEntites)
-    for infos in deduct:
-        allInferences.append([["| oui |", mot1, infos[0], infos[1], "&", infos[1], relation, mot2, "|", infos[2]], int(infos[2]), "deductive"])
-
-    # relation transitive
-    trans = isRelDeductiveInductive(dataEnt, idRel, 20, transEntities)
-    for infos in trans:
-        allInferences.append([["| oui |", mot1, infos[0], infos[1], "&", infos[1], relation, mot2, "|", infos[2]], int(infos[2]), "transitive"])
-
-    syn = relSynonyme(data, dataEnt, 100)
+    # relation synonyme
+    syn = relSynonyme(data, dataEnt, relation, 100)
     for infos in syn:
-        allInferences.append([["| oui |", mot1, "synonyme de ", infos[0], relation, infos[1], "synonyme de ",  mot2, "|", infos[2]], int(infos[2]), "synonyme"])
+        # réduire le score comme il y a 3 poids cumulés au lieu de 2
+        score = int(infos[2])*0.66
+        allInferences.append(["| oui | " + mot1 + " synonyme de " + infos[0] + " & " + infos[0] + " " + relation + " " + infos[1] + " & " + infos[1]  + " synonyme de " + mot2, score])
 
-# format : [explication, poids, inférence]
+
+
+
+
 allInferences = []
 
-# Inférences + affichage :
 # regarder si la relation existe de base + si poids négatif, skip la suite
 relBase = isRelation(dataEnt, idMot1, idMot2, idRel)
 if relBase[0] != "":
-    if(int(relBase[1]) > -100):
-        allInferences.append([["| oui |", mot1, relation, mot2, 5, "base"], int(relBase[1]), "base"])
+    if(int(relBase[1]) > 0):
         # suite des inférences
         resteInference()
     else:
-        print("Relation de base négative, pas d'inférences possibles")
+        print("1 | Non | Poids négatif : ", relBase[1])
 else:
     # si la relation de base n'existe pas : suite des inférences
     resteInference()
         
 
-    
+elapsed_time = (time.time() - start_time)
 
 
 # tri par score et affichage des inférences
 allInferences = sorted(allInferences,key=lambda x: x[1],reverse = True)
-for inf in allInferences:
-    print(inf[0])
+for i in range (len(allInferences)):
+    if i == 10:
+        break
+    print(str(i + 1) + " " + allInferences[i][0], "| ", allInferences[i][1])
 
+print("\nTemps d'exécution : ", elapsed_time, "s")
 
 
 
